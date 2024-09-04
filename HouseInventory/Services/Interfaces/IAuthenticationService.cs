b6c3df99d843db63240b54ad6ccd0fe95a23635f
vastamaa <vsztom@gmail.com>
2024-09-04 20:58:25 +0200

    Created a custom authentication service for registering.

---
+++ HouseInventory/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using HouseInventory.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+
+namespace HouseInventory.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<IdentityResult> RegisterUser(UserForRegistrationDto userRegistration);
+    }
+}
