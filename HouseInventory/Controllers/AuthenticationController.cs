35ca46137c8345948718ba827c741c0ff88cd76d
vastamaa <vsztom@gmail.com>
2024-09-04 21:00:26 +0200

    Added Register User POST endpoint.

---
+++ HouseInventory/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using HouseInventory.Models.DTOs;
+using HouseInventory.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseInventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            var result = await _authenticationService.RegisterUser(userForRegistration);
+
+            if (!result.Succeeded)
+            {
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+
+            return Created();
+        }
+    }
+}
